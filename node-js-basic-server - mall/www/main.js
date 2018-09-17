c7e603b71c92574b47f46a2d86d70d2bc5af33ea
LinaMattsson <linamattsson@live.se>
2018-09-17 13:14:22 +0200

    possible to match recipe with search input

--- node-js-basic-server - mall/www/main.js
+++ node-js-basic-server - mall/www/main.js
@@ -1,16 +1,43 @@
-$.getJSON('/autocomplete-ingredient-name/bac', function(recipes){
-    console.log(recipes);
+// $.getJSON('/autocomplete-ingredient-name/bac', function(recipes){
+    // console.log(recipes);
+// })
 
-})
-
-let recipes = [];
+// let recipes = [];
 
 async function start() {
     recipes = await $.getJSON('/recipe.json').catch(console.err);
     console.log(recipes);
     // let recipes2 = await $.getJSON('/recipe2.json');
-
 }
+$('#search-recipe').on('click', async function(){
+    let searchinput = $('#input-search-recipe').val().toLowerCase();
+    recipes = await $.getJSON('/recipe.json').catch(console.err);
+    let match = false;
+    let result;
+    for(let r of recipes){
+        if(searchinput == r.name.toLowerCase()){
+            console.log(r.name)
+            match = true;
+            result = r;
+        }    
+    }
+
+    if(!match){
+     console.log("Inget recept med det namnet hittades")
+    }
+    if(result){
+    $('.result').text(result.name);
+    let ingredientlist;
+    // for(let i of result.ingredienser){
+    //     ingredientlist += in;
+    // }
+    
+    $('.result').append(result.name);
+
+
+    }
+    
+})
 
 
 start();
